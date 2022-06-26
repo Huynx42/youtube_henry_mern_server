ceb88bfd8e97580a092501a8bf01c4ee0c0c5f84
Nguyen Xuan Huy <huynx42@due.edu.vn>
2022-06-26 14:47:17 +0700

    2:19:00

--- client/src/components/auth/LoginForm.js
+++ client/src/components/auth/LoginForm.js
@@ -1,17 +1,27 @@
 import Button from 'react-bootstrap/Button';
-import FormGroup from 'react-bootstrap/esm/FormGroup';
 import Form from 'react-bootstrap/Form';
 import {Link} from 'react-router-dom';
+import {useState, useContext} from 'react';
+import {AuthContext} from '../../contexts/AuthContext';
 
 const LoginForm = () => {
+    const {loginForm, setLoginForm} = useState({
+        username: '',
+        password: '',
+    })
+
+    const {username, password} = loginForm;
+
+    const onChangeLoginForm = event => setLoginForm({...loginForm, [event.target.name]: event.target.value})
+
     return (
         <>
         <Form className="my-4">
             <Form.Group>
-                <Form.Control type='text' placeholder='username' name='username' required />
+                <Form.Control type='text' placeholder='username' name='username' required value={username} onChange={onChangeLoginForm}/>
             </Form.Group>
             <Form.Group className='mt-2'>
-                <Form.Control type='password' placeholder='password' name='password' required />
+                <Form.Control type='password' placeholder='password' name='password' required value={password} onChange={onChangeLoginForm}/>
             </Form.Group>
             <Button className='mt-2' variant='success' type='submit'>Login</Button>
         </Form>
